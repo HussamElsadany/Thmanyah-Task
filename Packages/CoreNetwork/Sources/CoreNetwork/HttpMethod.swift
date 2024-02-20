5c065f447f7d8fc2a29c7f40af8dac7f51a32239
HussamElsadany <hussam.elsadany@gmail.com>
2024-02-21 01:07:16 +0200

    feat: create network client package

---
+++ Packages/CoreNetwork/Sources/CoreNetwork/HttpMethod.swift
@@ -0,0 +1,18 @@
+//
+//  HttpMethod.swift
+//
+//
+//  Created by Hussam Elsadany on 12/01/2024.
+//
+
+public enum HttpMethod: String {
+    case get     = "GET"
+    case post    = "POST"
+    case patch   = "PATCH"
+    case delete  = "DELETE"
+    case connect = "CONNECT"
+    case head    = "HEAD"
+    case options = "OPTIONS"
+    case put     = "PUT"
+    case trace   = "TRACE"
+}
