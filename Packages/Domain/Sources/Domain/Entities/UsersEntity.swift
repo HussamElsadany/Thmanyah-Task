7b597d55f3ce0652504edef0e468351d52e03273
HussamElsadany <hussam.elsadany@gmail.com>
2024-02-24 14:40:01 +0200

    feat: add entities

---
+++ Packages/Domain/Sources/Domain/Entities/UsersEntity.swift
@@ -0,0 +1,29 @@
+//
+//  UsersEntity.swift
+//
+//
+//  Created by Hussam Elsadany on 24/02/2024.
+//
+
+public struct UsersEntity: Codable {
+    public let id: Int
+    public let name, username, email: String
+    public let address: AddressEntity
+    public let phone, website: String
+    public let company: CompanyEntity
+}
+
+extension UsersEntity {
+    public struct AddressEntity: Codable {
+        public let street, suite, city, zipcode: String
+        public let geo: GeoEntity
+    }
+    
+    public struct GeoEntity: Codable {
+        public let lat, lng: String
+    }
+
+    public struct CompanyEntity: Codable {
+        public let name, catchPhrase, bs: String
+    }
+}
