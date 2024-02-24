25bfa36dad933f773fb01b53825a3e8417f6e82c
HussamElsadany <hussam.elsadany@gmail.com>
2024-02-24 14:22:09 +0200

    feat: add api's

--- Packages/ThmanyahAPI/Sources/ThmanyahAPI/AlbumsListEndpoint.swift
+++ Packages/ThmanyahAPI/Sources/ThmanyahAPI/AlbumsListEndpoint.swift
@@ -1,5 +1,5 @@
 //
-//  PlayListEndpoint.swift
+//  AlbumsListEndpoint.swift
 //
 //
 //  Created by Hussam Elsadany on 21/02/2024.
@@ -8,19 +8,25 @@
 import Foundation
 import CoreNetwork
 
-public enum PlayListEndpoint {
-    case getPlayList
+public enum AlbumsListEndpoint {
+    case getUsers
+    case getAlbums
+    case getPhotos
 }
 
-extension PlayListEndpoint: HTTPEndpoint {
+extension AlbumsListEndpoint: HTTPEndpoint {
     public var baseURL: String {
         CommonMovieService.baseURL
     }
     
     public var path: String {
         switch self {
-        case .getPlayList:
-            return "/api/playlist"
+        case .getUsers:
+            return "/users"
+        case .getAlbums:
+            return "/albums"
+        case .getPhotos:
+            return "/photos"
         }
     }
     
